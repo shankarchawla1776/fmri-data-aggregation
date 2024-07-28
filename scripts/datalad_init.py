e68f2740e6d706fb7ef53e7ff582f33603877d26
shankarchawla1776 <shankarchawla9101112@gmail.com>
2024-07-28 14:27:58 -0700

    Use datalad get method/switch to h5 outputs

--- scripts/datalad_init.py
+++ scripts/datalad_init.py
@@ -1,5 +1,4 @@
 import datalad.api as dl 
-from main import process_data 
 
 def datalad_init(url, local_path):
     data = dl.install(path=local_path, source=url)
@@ -11,6 +10,7 @@ if __name__ == "__main__":
     url = "https://github.com/OpenNeuroDatasets/ds002345"
     local_path = "./ds002345"
 
+
     dat = datalad_init(url, local_path)
 
-    print("files loaded")
+    print("files loaded") # nii.gz files are saved as symlinks
--- scripts/main.py
+++ scripts/main.py
@@ -1,42 +1,133 @@
-import numpy as np 
+import numpy as np
 import nibabel as nib
-import os 
-import subprocess
-from to_dropbox import upload_data 
+import os
+import logging
+import datalad.api as dl
+import dropbox
+import io
+import h5py
 
-def process_data(root): 
-    data = {}
+from dropbox.files import WriteMode
+from dropbox.exceptions import ApiError
 
-    for i in os.listdir(root):
-        if i.startswith('subject-'):
-            func = os.path.join(root, i, 'func')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+# def to_dropbox(file, dbx_path, access_token, chunk_size=16*1024*1024):
+def to_dropbox(file, dbx_path, access_token, chunk_size):
+
+
+    dbx = dropbox.Dropbox(access_token)
+
+
+    size = file.getbuffer().nbytes
+    
+    if size <= chunk_size: 
+        dbx.files_upload(file.getvalue(), dbx_path, mode=WriteMode("overwrite"))
+    else:
+        upload_start_result = dbx.files_upload_session_start(file.read(chunk_size))
+
+        # UploadSessionCursor tracks the progress
+        cursor = dropbox.files.UploadSessionCursor(session_id=upload_start_result.session_id,
+                                                   offset=file.tell())
+        
+
+        commit = dropbox.files.CommitInfo(path=dbx_path, mode=WriteMode("overwrite")) 
+
+        while file.tell() < size:
+            if (size - file.tell()) <= chunk_size:
+                dbx.files_upload_session_finish(file.read(chunk_size),
+                                                cursor,
+                                                commit)
+            else:
+                dbx.files_upload_session_append(file.read(chunk_size),
+                                                cursor.session_id,
+                                                cursor.offset)
+                cursor.offset = file.tell()
+            
+            # Log progress
+            progress = (file.tell() / size) * 100
+            logging.info(f"Progress: {progress:.2f}%")
+
+    logging.info(f"Uploaded: {dbx_path}")
+
+
+def process_data(root, app_token):
+    dataset = dl.Dataset(root)
+
+    per_task = {}
+
+    subjects = [i for i in os.listdir(root) if i.startswith('sub-')][:10]  # number of subjects to compute
+
+    for i in subjects:
+
+        # ---- needs to be updated for the preprocessed data repo ---- 
+        path = os.path.join(root, i)
+        func = os.path.join(path, 'func')
+
+        if os.path.exists(func):
+
             for j in os.listdir(func):
-                name = j.replace(i.replace('subject-', 'sub-'), '').replace('_bol.nii.gz', '')
-                path = os.path.join(func, j)
+                if j.endswith('bold.nii.gz'):
 
-                if os.path.exists(path):
-                    if name not in data: 
-                        data[name] = []
-                    data[name].apapend(nib.load(path).get_fdata())
+                    try:
+                        path = os.path.join(func, j)
+                        task = j.split('task-')[1].split('_')[0]
+                        
+                        # Use datalad get to retrieve the file content
+                        result = dataset.get(path, result_renderer='disabled')
 
-    for k in data: 
-        data[k] = np.array(data[k])
-    return data 
+                        if result:
+                            img = nib.load(path)
+                            data = img.get_fdata()
+                            
+                            if task not in per_task:
+                                per_task[task] = []
+                            per_task[task].append(data)
+                            
+                            logging.info(f"Processed: {path}")
+                            
+                            # so that files do not get stored locally
+                            dataset.drop(path, check=False)
+                        else:
+                            
+                            raise Exception("Error with datalad get")
+                    except Exception as e:
+                        logging.error(f"Error for file {path}: {str(e)}")
+
+
+    for k, m in per_task.items():
+        try:
+            data = np.concatenate(m, axis=-1)
+            
+            # a bytesIO object needs to hold the h5 file
+            h5 = io.BytesIO()
+
+            with h5py.File(h5, 'w') as f:
+                f.create_dataset('task_data', data=path, compression='gzip', compression_opts=9)
+            h5.seek(0)
+        
+            dbx_path = f"/task-{k}_bold.h5"
+            
+            # might want to edit for the preprocessed data 
+            chunk_size=16*1024*1024
+            to_dropbox(h5, dbx_path, app_token, chunk_size)
+            
+            logging.info(f"data for task: {k}")
+        except Exception as e:
+
+            logging.error(f"Error for task {k}: {str(e)}")
 
 if __name__ == "__main__":
-    local_path = "./ds002345"
+    local_path = os.path.abspath("../ds002345")
     app_token = os.getenv("DROPBOX_APP_TOKEN")
-    data = process_data(local_path)
 
-    for i, j in data.items(): 
-
-        np.save(f"{i}_data.npy", j)
-        print(j.shape)
-        # just put in whatever dropbox filename you have
-        box_path = f"fmri_data/{i}_data.npy"
-        
-        upload_data(f"{i}_data.npy", box_path, app_token)
-
-
-
-
+    if not os.path.exists(local_path):
+        logging.error(f"{local_path} not found")
+        exit(1)
+    
+    # change to the dataset dir 
+    os.chdir(local_path)
+    
+    process_data('.', app_token)
+    logging.info("done")
